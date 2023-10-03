8a386da2d7d80d5256157ba2ff19088032d64d42
zellygao <zg2311@student.cis.edu.hk>
2023-10-03 08:40:44 +0800

    second part

--- src/TextBasedGame.java
+++ src/TextBasedGame.java
@@ -13,35 +13,74 @@ public class TextBasedGame {
         System.out.println("What is your budget for dinner?");
         int budget = scanner.nextInt();
         ingredients(cusine);
-        System.out.println("Now that you have your list, you can go to the supermarket to buy the ingredients. You will be asked to estimate the price of each ingredient, and in the end you will find out if you successfully purchased all the ingredients for dinner tonight! Enter 1 if you would like to proceed.");
-
-        if(scanner.nextInt() == 1){
-
+        System.out.println("Now that you have your list, you can go to the supermarket to buy the ingredients. You will be asked to estimate the price of each ingredient, and in the end you will find out if you successfully purchased all the ingredients for dinner! Enter 1 to proceed.");
+        int proceed = scanner.nextInt();
+        if(proceed==1){
+            int es= scanner
+            asianguess(cusine,scanner.nextInt());
+        }
         }
 
-        String nori = "1 pack of nori";
-        String salmon = "50grams of salmon";
-        String rice = "1kg pack of rice";
-        String cucumber = "1 small cucumber";
 
-        int estimate;
-    }
     public static void ingredients (int cusine){
         if (cusine == 1){
-            System.out.println("You will be making sushi tonight. Here is your ingredients list:" + "\nnori sheet" + "\n50g fresh salmon" + "\n50g sushi rice" + "\n1 cucumber" );
+            System.out.println( "You will be making sushi tonight. Here is your ingredients list:" + "\n1 nori sheet" + "\n50g fresh salmon" + "\n50g sushi rice" + "\n1 cucumber" );
         }
         else if (cusine == 2){
             System.out.println("You will making spaghetti bolognese tonight. Here is your ingredients list:" + "\n 100g minced beef" + "\n 1 can tomato puree" + "\n 100g dried spaghetti" + "\n 1 onion");
         }
-        //else if (cusine != 1 || cusine !=2){
-            //System.out.println("Sorry, you must input either 1 or 2");
-        //}
+        else if (cusine != 1 || cusine !=2) {
+            System.out.println("Sorry, you must input either 1 or 2");
+        }
     }
 
-    public static pricecheck(int estimate){
-        if(estimate <= scanner.nextInt() )
+    public static String ask (String food){
+        return "How much do you think" + food + "costs?";
     }
-    public static void ask (String food){
-        System.out.println("How much do you think" + food + "costs?");
+
+    public static String pricecheck (int estimate, int real, String food){
+        if (estimate == real){
+            return "Correct!" + food + "costs" + real + "dollars!";
+        }
+        else if(Math.abs(estimate - real) <= 10){
+            return "You were close!" + food + "costs" + real + "dollars!";
+            //
+        }
+        return "Uhoh, you were not close."+ food + "costs" + real + "dollars.";
     }
+
+    public static void asianguess(int cusine, Scanner scanner){
+        int nori = 200;
+        int rice = 60;
+        int salmon = 50;
+        int cucumber = 5;
+
+        if(cusine ==1){
+            ask("1 pack of nori");
+            int total = scanner.nextInt();
+            pricecheck(scanner.nextInt(), nori, "1 pack of nori");
+
+            ask ("a 1kg pack of japanese rice");
+            total = total + scanner.nextInt();
+            pricecheck(scanner.nextInt(),rice,"a 1kg pack of japanese rice");
+
+            ask ("1 pack of salmon");
+            total = total + scanner.nextInt();
+            pricecheck(scanner.nextInt(),salmon,"1 pack of salmon");
+
+            ask ("1 cucumber");
+            total = total + scanner.nextInt();
+            pricecheck(scanner.nextInt(),cucumber,"1 cucumber");
+        }
+    }
+    public static void result () {
+        // tell them if they successfully
+
+    }
+
 }
+
+// use * for multiplying number of ppl and ingredients for dinner
+// use / for ?
+// use % for ?
+// use - for ?
