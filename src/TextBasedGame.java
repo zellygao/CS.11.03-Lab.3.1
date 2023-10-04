00c87cc71a108e06a28b33fe5e12fd02a2e72b8d
zellygao <zg2311@student.cis.edu.hk>
2023-10-04 19:04:15 +0800

    everything except cant update the score every time

--- src/TextBasedGame.java
+++ src/TextBasedGame.java
@@ -2,85 +2,138 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class TextBasedGame {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Elly's Supermarket Guesser! What is your name?");
         String name = scanner.next();
         System.out.println("Welcome " + name + "! You will be cooking dinner tonight, what would you like? Enter 1 for Asian, or 2 for Western.");
-        int cusine = scanner.nextInt();
-        System.out.println("How many people will you be cooking for?");
-        int quota = scanner.nextInt();
-        System.out.println("What is your budget for dinner?");
-        int budget = scanner.nextInt();
-        ingredients(cusine);
-        System.out.println("Now that you have your list, you can go to the supermarket to buy the ingredients. You will be asked to estimate the price of each ingredient, and in the end you will find out if you successfully purchased all the ingredients for dinner! Enter 1 to proceed.");
+        int cuisine = scanner.nextInt();
+        ingredients(cuisine);
+        System.out.println("\n Now that you have your list, you can go to the supermarket to buy the ingredients. You will be asked to estimate the price of each ingredient. \n Here is how the scoring system works:");
+        System.out.println(points());
+        System.out.println("\n Please enter 1 to proceed!");
         int proceed = scanner.nextInt();
-        if(proceed==1){
-            int es= scanner
-            asianguess(cusine,scanner.nextInt());
-        }
+
+        int score=0;
+
+        if (proceed == 1) {
+            asianguess(cuisine, scanner);
+            westernguess(cuisine, scanner);
         }
 
+        System.out.println("Enter 1 to view your results.");
+        proceed = scanner.nextInt();
+        if(proceed==1) {
+            System.out.println(score(score));
+            System.out.println("You scored " + result(score) + " %");
+            System.out.println(passorfail(score));
+        }
 
-    public static void ingredients (int cusine){
-        if (cusine == 1){
-            System.out.println( "You will be making sushi tonight. Here is your ingredients list:" + "\n1 nori sheet" + "\n50g fresh salmon" + "\n50g sushi rice" + "\n1 cucumber" );
-        }
-        else if (cusine == 2){
-            System.out.println("You will making spaghetti bolognese tonight. Here is your ingredients list:" + "\n 100g minced beef" + "\n 1 can tomato puree" + "\n 100g dried spaghetti" + "\n 1 onion");
-        }
-        else if (cusine != 1 || cusine !=2) {
-            System.out.println("Sorry, you must input either 1 or 2");
-        }
     }
 
-    public static String ask (String food){
-        return "How much do you think" + food + "costs?";
+    public static void ingredients(int cuisine) {
+        if (cuisine == 1) {
+                System.out.println("You will be making sushi tonight. Here is your ingredients list:" + "\n 1 nori sheet" + "\n 50g fresh salmon" + "\n 50g sushi rice" + "\n 1 cucumber");
+            }
+        else if (cuisine == 2) {
+           System.out.println("You will making spaghetti bolognese tonight. Here is your ingredients list:" + "\n 100 g minced beef" + "\n 1 jar of tomato sauce" + "\n 100g dried spaghetti" + "\n 1 onion");
+            }
+        //else if (cuisine != 1 || cuisine != 2) {
+         //   System.out.println("Sorry, you must input either 1 or 2");
+        //}
     }
 
-    public static String pricecheck (int estimate, int real, String food){
-        if (estimate == real){
-            return "Correct!" + food + "costs" + real + "dollars!";
-        }
-        else if(Math.abs(estimate - real) <= 10){
-            return "You were close!" + food + "costs" + real + "dollars!";
-            //
-        }
-        return "Uhoh, you were not close."+ food + "costs" + real + "dollars.";
+    public static String points (){
+        return "  3 points for a completely accurate estimate" + "\n  2 points for estimate <15% off from the actual price" + "\n  1 point for an estimate <50% off from the actual price, or if your estimate is a multiple of the actual price";
+
+    }
+    public static String ask(String food) {
+        return "\n" + "How much do you think " + food + " costs?";
     }
 
-    public static void asianguess(int cusine, Scanner scanner){
-        int nori = 200;
+    public static int pricecheck (int estimate, int real, String food, int score) {
+        if (estimate-real == 0){
+            System.out.println("Correct! " + food + " costs " + real + " dollars!");
+            score +=3;
+            System.out.println("Your current score:");
+            return score;
+        }
+        else if (real / estimate <= 1.15 && real / estimate >=0.85) {
+            System.out.println("You were close! " + food + " costs " + real + " dollars!");
+            score+=2;
+            System.out.println("Your current score:");
+            return score;
+        }
+        else if(real / estimate <=1.5 && real / estimate >=0.5 || real%estimate == 0){
+            System.out.println("You were kind of close. " + food + " costs " + real + " dollars.");
+            score+=1;
+            System.out.println("Your current score:");
+            return score;
+        }
+        System.out.println("Uhoh, you were not close. " + food + " costs " + real + " dollars.");
+        System.out.println("Your current score:");
+        return score;
+    }
+
+    public static void asianguess(int cuisine, Scanner scanner) {
+        int nori = 150;
         int rice = 60;
         int salmon = 50;
         int cucumber = 5;
+        int score = 0;
 
-        if(cusine ==1){
-            ask("1 pack of nori");
-            int total = scanner.nextInt();
-            pricecheck(scanner.nextInt(), nori, "1 pack of nori");
+        if (cuisine == 1) {
+            proceed("1 pack of nori", nori, scanner, score);
+            proceed("1kg pack of japanese rice", rice, scanner, score);
+            proceed("50g of salmon", salmon, scanner, score);
+            proceed("1 cucumber", cucumber, scanner, score);
 
-            ask ("a 1kg pack of japanese rice");
-            total = total + scanner.nextInt();
-            pricecheck(scanner.nextInt(),rice,"a 1kg pack of japanese rice");
-
-            ask ("1 pack of salmon");
-            total = total + scanner.nextInt();
-            pricecheck(scanner.nextInt(),salmon,"1 pack of salmon");
-
-            ask ("1 cucumber");
-            total = total + scanner.nextInt();
-            pricecheck(scanner.nextInt(),cucumber,"1 cucumber");
         }
     }
-    public static void result () {
-        // tell them if they successfully
 
+    public static void proceed (String food, int real, Scanner scanner, int score) {
+        System.out.println(ask(food));
+        int estimate = scanner.nextInt();
+        System.out.println(pricecheck(estimate, real, food, score));
     }
 
+    public static void westernguess(int cuisine, Scanner scanner) {
+
+        int beef = 50;
+        int spaghetti = 40;
+        int onion = 6;
+        int tomato = 40;
+        int score = 0;
+
+        if (cuisine == 2) {
+            proceed("50g of minced beef", beef, scanner, score);
+            proceed("1 pack of dried spaghetti", spaghetti, scanner, score);
+            proceed("1 white onion", onion, scanner, score);
+            proceed("1 jar of tomato sauce", tomato, scanner, score);
+        }
+    }
+   public static String score (int score){
+     return "You scored" + score + "/12";
 }
 
-// use * for multiplying number of ppl and ingredients for dinner
-// use / for ?
-// use % for ?
+    public static Double result (int score) {
+        return (double) score / 12 * 100;
+    }
+
+    public static String passorfail (int score){
+        if(score/12 >=0.8){
+            return "Congratulations! You successfully purchased all the ingredients required for dinner. Enjoy!";
+        }
+        return "Sorry, but you couldn't purchase all the ingredients required for dinner and now you are hungry. :( " ;
+    }
+}
+
+
+// use * for the overall percentage
+// use / for dividing the total score
+// use % for multiple
 // use - for ?
+// logical operator x 1 done
+// comparison operator x 2 done
+// 10 if statements done
+// 10 methods done
